d1b42a4d300f1c8b3ae8241817a76c34ab9c0ce2
Fabian Schmid <fs@studer-raimann.ch>
2015-10-05 10:55:40 +0200

    new plugin version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = "xlel";
-$version = "1.0.08";
-$ilias_min_version = "4.2.0";
+$version = "2.0.00";
+$ilias_min_version = "5.0.0";
 $ilias_max_version = "5.1.999";
 $responsible = "Fabian Schmid";
 $responsible_mail = "fs@studer-raimann.ch";
