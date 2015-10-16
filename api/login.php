e1c2cf94caaa6db823d14e5612b7607b7a34cf61
Fabian Schmid <fs@studer-raimann.ch>
2015-10-16 21:28:14 +0200

    login-service

---
+++ api/login.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Loggs the User in and redirects
+ */
+$base_dir = realpath(dirname(__FILE__) . '/../../../../../../../..');
+chdir($base_dir);
+$_POST['username'] = $_GET['username'];
+$_POST['password'] = $_GET['password'];
+
+if (isset($_GET["client_id"])) {
+	setcookie("ilClientId", $_GET["client_id"], 0, '/', '');
+	$_COOKIE["ilClientId"] = $_GET["client_id"];
+}
+$GLOBALS['COOKIE_PATH'] = "/";
+require_once("Services/Init/classes/class.ilInitialisation.php");
+ilInitialisation::initILIAS();
+$str = strstr($_SERVER['SCRIPT_URI'], 'Customizing', true) . $_GET['goto'];
+ilUtil::redirect($str);
