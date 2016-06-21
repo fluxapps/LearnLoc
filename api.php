371223e7c7931369268772c539ee550d72a8ea3b
Fabian Schmid <fs@studer-raimann.ch>
2016-06-21 09:27:58 +0200

    code format

--- api.php
+++ api.php
@@ -18,23 +18,23 @@ require 'vendor/autoload.php';
 
 $modes = array(
 	'development',
-	'production',
+	'production'
 );
 $mode = isset($_GET['mode']) && in_array($_GET['mode'], $modes) ? $_GET['mode'] : 'development';
 //$mode = 'production';
 
 $app = new Slim(array(
-	'mode' => $mode,
+	'mode' => $mode
 ));
 
 // Settings applied when booting in dev or prod
 $settings = array(
 	'development' => array(
-		'debug' => true,
+		'debug' => true
 	),
-	'production'  => array(
-		'debug' => false,
-	),
+	'production' => array(
+		'debug' => false
+	)
 );
 
 $app->configureMode('development', function () use ($app, $settings) {
@@ -52,6 +52,7 @@ $env = $app->environment();
 if ($_REQUEST['get_request'] === 'true') {
 	$env['HTTP_X_HTTP_METHOD_OVERRIDE'] = 'GET';
 }
+
 
 // Disable CORS
 $app->response->headers->set('Access-Control-Allow-Origin', '*');
@@ -75,7 +76,7 @@ function response(array $data) {
  */
 function imageResponse($image_path) {
 	$app = Slim::getInstance();
-	if (!is_file($image_path)) {
+	if (! is_file($image_path)) {
 		$app->response->setStatus(404);
 	} else {
 		$app->response->headers()->set('Content-Type', 'image/jpeg');
@@ -89,21 +90,18 @@ function imageResponse($image_path) {
 
 $app->get('/', function () use ($app) {
 	response(array(
-		'GET /ping'                     => 'is Alive?',
-		'GET /courses'                  => 'List of courses',
-		'GET /course/:id/locations'     => 'Returns all locations (and folders) of the given course ref-ID',
-		'GET /location/:id/image'       => 'Returns the location image',
-		'GET /location/:id/image_data'  => 'Returns the location image',
-		'GET /location/:id/thumb'       => 'Returns a thumbnail version of the location image',
-		'GET /location/:id/thumb_data'  => 'Returns a thumbnail version of the location image',
-		'GET /location/:id/header'      => 'Returns a header version of the location image',
-		'GET /location/:id/header_data' => 'Returns a header version of the location image',
-		'GET /campusTour'               => 'Returns all locations (and folders) from the campus tour',
-		'GET /location/:id/comments'    => 'Returns all comments for the location. Pass GET-Parameters "start" and "count" for paging',
-		'GET /comment/:id/image'        => 'Returns image of comment',
-		'GET /comment/:id/thumb'        => 'Returns thumbnail image of comment',
-		'POST /location/:id/comment'    => 'Create a new Comment. Post parameters (url-encoded): title, body, image, parent_id',
-		'POST /course/:id'              => 'Create a new location under the given course (Ref-ID). Post parameters (url-encoded): title, description, latitude, longitude, image, address',
+		'GET /ping' => 'is Alive?',
+		'GET /courses' => 'List of courses',
+		'GET /course/:id/locations' => 'Returns all locations (and folders) of the given course ref-ID',
+		'GET /location/:id/image' => 'Returns the location image',
+		'GET /location/:id/thumb' => 'Returns a thumbnail version of the location image',
+		'GET /location/:id/header' => 'Returns a header version of the location image',
+		'GET /campusTour' => 'Returns all locations (and folders) from the campus tour',
+		'GET /location/:id/comments' => 'Returns all comments for the location. Pass GET-Parameters "start" and "count" for paging',
+		'GET /comment/:id/image' => 'Returns image of comment',
+		'GET /comment/:id/thumb' => 'Returns thumbnail image of comment',
+		'POST /location/:id/comment' => 'Create a new Comment. Post parameters (url-encoded): title, body, image, parent_id',
+		'POST /course/:id' => 'Create a new location under the given course (Ref-ID). Post parameters (url-encoded): title, description, latitude, longitude, image, address'
 	));
 });
 
@@ -129,55 +127,29 @@ $app->get('/course/:id/locations', function ($id) use ($app) {
 
 $app->get('/location/:id/image', function ($id) use ($app) {
 	$service = new LocationImageService($id, array(
-		'w'    => 960,
-		'h'    => 960,
-		'crop' => false,
+		'w' => 960,
+		'h' => 960,
+		'crop' => false
 	));
 	imageResponse($service->getResponse());
-});
-
-$app->get('/location/:id/image_data', function ($id) use ($app) {
-	$service = new LocationImageService($id, array(
-		'w'    => 960,
-		'h'    => 960,
-		'crop' => false,
-	), true);
-	$service->getResponse();
 });
 
 $app->get('/location/:id/header', function ($id) use ($app) {
 	$service = new LocationImageService($id, array(
-		'w'    => 960,
-		'h'    => 260,
-		'crop' => false,
+		'w' => 960,
+		'h' => 260,
+		'crop' => false
 	));
 	imageResponse($service->getResponse());
-});
-$app->get('/location/:id/header_data', function ($id) use ($app) {
-	$service = new LocationImageService($id, array(
-		'w'    => 960,
-		'h'    => 260,
-		'crop' => false,
-	), true);
-	$service->getResponse();
 });
 
 $app->get('/location/:id/thumb', function ($id) use ($app) {
 	$service = new LocationImageService($id, array(
-		'w'    => 64,
-		'h'    => 64,
-		'crop' => true,
+		'w' => 64,
+		'h' => 64,
+		'crop' => true
 	));
 	imageResponse($service->getResponse());
-});
-
-$app->get('/location/:id/thumb_data', function ($id) use ($app) {
-	$service = new LocationImageService($id, array(
-		'w'    => 64,
-		'h'    => 64,
-		'crop' => true,
-	), true);
-	$service->getResponse();
 });
 
 $app->get('/campusTour', function () use ($app) {
@@ -194,18 +166,18 @@ $app->get('/location/:id/comments', function ($id) use ($app) {
 
 $app->get('/comment/:id/thumb', function ($id) use ($app) {
 	$service = new CommentImageService($id, array(
-		'w'    => 64,
-		'h'    => 64,
-		'crop' => true,
+		'w' => 64,
+		'h' => 64,
+		'crop' => true
 	));
 	imageResponse($service->getResponse());
 });
 
 $app->get('/comment/:id/image', function ($id) use ($app) {
 	$service = new CommentImageService($id, array(
-		'w'    => 960,
-		'h'    => 960,
-		'crop' => false,
+		'w' => 960,
+		'h' => 960,
+		'crop' => false
 	));
 	imageResponse($service->getResponse());
 });
@@ -214,7 +186,7 @@ $app->post('/location/:id/comment', function ($id) use ($app) {
 	$parent_id = $app->request()->post('parent_id');
 	$data = array(
 		'title' => $app->request()->post('title'),
-		'body'  => $app->request()->post('body'),
+		'body' => $app->request()->post('body'),
 		'image' => $app->request()->post('image'),
 	);
 	$service = new CreateCommentService($id, $parent_id, $data);
@@ -223,12 +195,12 @@ $app->post('/location/:id/comment', function ($id) use ($app) {
 
 $app->post('/course/:id', function ($id) use ($app) {
 	$data = array(
-		'title'       => $app->request()->post('title'),
+		'title' => $app->request()->post('title'),
 		'description' => $app->request()->post('description'),
-		'image'       => $app->request()->post('image'),
-		'latitude'    => $app->request()->post('latitude'),
-		'longitude'   => $app->request()->post('longitude'),
-		'address'     => $app->request()->post('address'),
+		'image' => $app->request()->post('image'),
+		'latitude' => $app->request()->post('latitude'),
+		'longitude' => $app->request()->post('longitude'),
+		'address' => $app->request()->post('address'),
 	);
 	$service = new CreateLocationService($id, $data);
 	response($service->getResponse());
